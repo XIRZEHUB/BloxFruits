7999e70462a448142444b1c39ca88b53a8b9ff3e
XIRZEHUB <nattanonwimonrat@gmail.com>
2024-05-13 11:26:45 +0700

    Update Kaitan.lua

--- Kaitan.lua
+++ Kaitan.lua
@@ -1,5 +1,11 @@
-function New()
-    print(1)
+join = game.Players.localPlayer.Neutral == false
+if not join then
+	repeat wait()
+		pcall(function()
+			join = game.Players.localPlayer.Neutral == false
+			for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.TextButton.Activated)) do
+        v.Function()
+      end
+		end)
+	until join == true and game.Players.LocalPlayer.Team ~= nil and game:IsLoaded() 
 end
-
-New() 
